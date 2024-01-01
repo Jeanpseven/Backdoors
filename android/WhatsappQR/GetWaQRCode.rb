b8e4beebc13e84feb39918a0c650940376d1a49e
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-01-01 13:05:14 -0300

    Update GetWaQRCode.rb

--- android/WhatsappQR/GetWaQRCode.rb
+++ android/WhatsappQR/GetWaQRCode.rb
@@ -1,17 +1,40 @@
+
+
 #!/usr/bin/env ruby
 
-# Detecta o navegador padrão
-navegador = if system('command -v google-chrome &> /dev/null')
-              `command -v google-chrome`.chomp
-            elsif system('command -v firefox &> /dev/null')
-              `command -v firefox`.chomp
-            else
-              puts "Nenhum navegador suportado encontrado."
-              exit 1
-            end
+require 'msf/core'
 
-# Substitua 'https://web.whatsapp.com/' pelo link do WhatsApp Web
-url = "https://web.whatsapp.com/"
+class MetasploitModule < Msf::Post
 
-# Abre o navegador com o link do WhatsApp Web
-system("#{navegador} #{url}")
+ include Msf::Post::Common
+ include Msf::Post::File
+
+ # Configuração do módulo
+ def initialize(info = {})
+    super(update_info(info,
+                      'Name' => 'Get WhatsApp Web QR',
+                      'Description' => 'Abre o navegador com o link do WhatsApp Web',
+                      'Author' => 'Jeanpseven',
+                      'Platform' => 'Multi',
+                      'Arch' => ARCH_X86))
+ end
+
+ # Ação principal do módulo
+ def run
+    # Detecta o navegador padrão
+    navegador = if cmd_exec('command -v google-chrome &> /dev/null')
+                 cmd_exec('command -v google-chrome').chomp
+                elsif cmd_exec('command -v firefox &> /dev/null')
+                 cmd_exec('command -v firefox').chomp
+                else
+                 print_error("Nenhum navegador suportado encontrado.")
+                 return
+                end
+
+    # Substitua 'https://web.whatsapp.com/' pelo link do WhatsApp Web
+    url = "https://web.whatsapp.com/"
+
+    # Abre o navegador com o link do WhatsApp Web
+    cmd_exec("#{navegador} #{url}")
+ end
+end
